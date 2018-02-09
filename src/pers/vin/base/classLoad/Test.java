1dca1891b2eb9d285c157ab800f8c2f3ea228d5d
vin <731315455@qq.com>
2018-02-09 18:10:09 +0800

    + d , test singleTon  with class load

---
+++ src/pers/vin/base/classLoad/Test.java
@@ -0,0 +1,47 @@
+package pers.vin.base.classLoad;
+
+/**
+ * Created by vin on 25/01/2018.
+ */
+class SingleTon {
+
+    private static SingleTon singleTon = new SingleTon();
+
+    public static int count1;
+    public static int count2 = 0;
+    // init by default order  , so count1=1,count2=1
+    // private static SingleTon singleTon = new SingleTon();
+
+    private SingleTon() {
+
+        count1++;
+        count2++;
+
+        System.out.println("constructor after ++ count1= " + count1);
+        System.out.println("constructor after ++ count2= " + count2);
+
+    }
+
+    static {
+        System.out.println("static count1= " + count1);
+        System.out.println("static count2= " + count2);
+
+    }
+
+    static void print() {
+        System.out.println("static print");
+    }
+
+    public static SingleTon getInstance() {
+        return singleTon;
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        SingleTon singleTon = SingleTon.getInstance();
+        SingleTon.print();
+        System.out.println("count1=" + singleTon.count1);
+        System.out.println("count2=" + SingleTon.count2);
+    }
+}
